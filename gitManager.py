d57f66f9e1c21cc2abb2b72ba89ff46a75aecbf3
Ben Doherty <ben@notionparallax.co.uk>
2016-12-19 12:47:14 -0800

    :pig: new toy :goat:

---
+++ gitManager.py
@@ -0,0 +1,30 @@
+# This file just does a `git status` on all the
+# folders in the main git folder.
+# It'll tell you which ones need to have things commited
+
+
+import os
+import git
+
+rootdir = '../'
+dirList = os.listdir(rootdir)
+# print dirList
+
+dirsToCommit = []
+dirsToGit = []
+
+for thisDir in dirList:
+	try:
+		s = git.cmd.Git(rootdir+thisDir).status()
+		if "Changes not staged for commit" in s:
+			print thisDir
+			print s,"\n"
+			dirsToCheck.append(thisDir)
+		else:
+			print thisDir,"good to go"
+	except:
+		print "***",thisDir,"isn't a git repo"
+		dirsToGit.append(thisDir)
+
+print "\ndirsToCommit:\n",dirsToCommit
+print "\ndirsToGit:\n",   dirsToGit
